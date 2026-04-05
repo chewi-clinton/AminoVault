7b2f5d8bb93ab84b56c7404671f0563ebc5048a1
chewi-clinton <suhchenwi48@gmail.com>
2026-04-05 08:03:25 +0100

    Add Dockerfile for deployment

--- AminoVault/src/pages/HomePage.jsx
+++ AminoVault/src/pages/HomePage.jsx
@@ -3,6 +3,10 @@ import Promo from "../pages/PromotionSection.jsx";
 import Hero from "../pages/HeroSection.jsx";
 import About from "../pages/AminoDetailsPage.jsx";
 import Benefits from "../pages/ResearchBenefits.jsx";
+
+import TrustedSection from "../pages/TrustedSection.jsx";
+import ResearchSection from "../pages/ResearchSection.jsx";
+import PuritySection from "../pages/PuritySection.jsx";
 const HomePage = () => {
   return (
     <>
@@ -10,6 +14,9 @@ const HomePage = () => {
       <Promo />
       <About />
       <Benefits />
+      <TrustedSection />
+      <ResearchSection />
+      <PuritySection />
     </>
   );
 };
